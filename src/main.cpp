10944b9f7852f15352696decdead0d4e8bc087d8
PRAN <prantokumar.net@gmail.com>
2024-08-13 20:02:47 +0600

    Reuploaded

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include<raylib.h>
+#define ScreenWidth 800
+#define ScreenHight 600
+int main(){
+  InitWindow(ScreenWidth,ScreenHight,  "Brick Games v1.0");
+  
+  
+  
+  
+  
+  while(!WindowShouldClose()){
+    BeginDrawing();
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
